97b89ee7c8881b419b03cf7171f5b601f47d8512
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-01 17:57:37 +0100

    feat: ensure CreateUserUseCase calls JwtAdapter.encrypt with correct user data

--- src/data/domain/encrypt.ts
+++ src/data/domain/encrypt.ts
@@ -1,5 +1,3 @@
-import { JSONType } from "@/data/model";
-
-export interface Encrypt {
-  encrypt (value: JSONType): string
+export interface Encrypt<P> {
+  encrypt (value: P): string
 }
--- src/data/features/create-user-use-case.ts
+++ src/data/features/create-user-use-case.ts
@@ -1,10 +1,14 @@
-import { CreateUser, Hash } from "@/data/domain";
+import { CreateUser, Hash, Encrypt } from "@/data/domain";
 import { CreateUserModel, UserModel } from "../model";
 
 export class CreateUserUseCase implements CreateUser {
-  constructor (private readonly hasher: Hash) {}
+  constructor (
+    private readonly hasher: Hash,
+    private readonly jwtAdapter: Encrypt<CreateUserModel>
+  ) {}
   async create(params: CreateUserModel): Promise<UserModel> {
     const hashedPassword = this.hasher.hash(params.password)
+    this.jwtAdapter.encrypt({...params, password: hashedPassword })
     return '' as any
   }
 }
