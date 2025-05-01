423f03c29ad9b862073c5d6382e7faa8c5db2b98
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-01 18:32:29 +0100

    feat: ensure CreateUserUseCase calls JwtAdapter.encrypt with correct userId after saved in repository

--- src/data/features/create-user-use-case.ts
+++ src/data/features/create-user-use-case.ts
@@ -5,7 +5,7 @@ import { CreateUserRepository } from "@/data/domain/create-user-repository";
 export class CreateUserUseCase implements CreateUser {
   constructor(
     private readonly hasher: Hash,
-    private readonly jwtAdapter: Encrypt<CreateUserModel>,
+    private readonly jwtAdapter: Encrypt<CreateUserModel | UserModel>,
     private readonly fsUserRepository: CreateUserRepository
   ) { }
 
@@ -15,9 +15,11 @@ export class CreateUserUseCase implements CreateUser {
   */
   async create(params: CreateUserModel): Promise<UserModel> {
     const hashedPassword = this.hasher.hash(params.password)
-    const token = this.jwtAdapter.encrypt({ ...params, password: hashedPassword })
-    const refreshToken = this.jwtAdapter.encrypt({ ...params, password: hashedPassword })
-    await this.fsUserRepository.create({ ...params, password: hashedPassword, token, refreshToken })
+    let token = this.jwtAdapter.encrypt({ ...params, password: hashedPassword })
+    let refreshToken = this.jwtAdapter.encrypt({ ...params, password: hashedPassword })
+    const userModel = await this.fsUserRepository.create({ ...params, password: hashedPassword, token, refreshToken })
+    token = await this.jwtAdapter.encrypt({ ...params, password: hashedPassword, id: userModel.id })
+    refreshToken = await this.jwtAdapter.encrypt({ ...params, password: hashedPassword, id: userModel.id })
     return '' as any
   }
 }
