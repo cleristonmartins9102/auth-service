56af0c8a75df4183d90967f8253e8abfbf447d16
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-02 21:28:27 +0100

    refactor: added await on bcrypt tests

--- tests/infra/adapters/bcrypt-adapter.spec.ts
+++ tests/infra/adapters/bcrypt-adapter.spec.ts
@@ -7,24 +7,22 @@ jest.mock('bcrypt')
 
 describe('BcryptAdapter', () => {
   const fakePassword = faker.internet.password()
+  const spy = jest.spyOn(bcrypt, 'hash')
 
-  it('should call bcrypt.hash with correct values', () => {
-    const spy = jest.spyOn(bcrypt, 'hash')
+  it('should call bcrypt.hash with correct values', async () => {
     const sut = new BcryptAdapter()
 
-    sut.hash(fakePassword)
+    await sut.hash(fakePassword)
 
     expect(spy).toHaveBeenCalled()
     expect(spy).toHaveBeenCalledWith(fakePassword, 10)
   })
 
-  it('should returns the same value received from bcrypt.hash', () => {
-    const spy = jest.spyOn(bcrypt, 'hash') as any
-    spy.mockReturnValueOnce('hashedValue')
+  it.only('should returns the same value received from bcrypt.hash', async () => {
+    spy.mockReturnValueOnce('hashedValue' as any)
     const sut = new BcryptAdapter()
 
-    const response = sut.hash(fakePassword)
-
+    const response = await sut.hash(fakePassword)
     expect(response).toBe('hashedValue')
   })
 })
