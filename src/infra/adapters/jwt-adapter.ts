4c187561f5609745d700b6c14c22ab5a5f4a78d2
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-02 16:08:25 +0100

    feat: ensure JwtAdapter.encrypt returns the same value received from jsonwebtoken.sign

--- src/infra/adapters/jwt-adapter.ts
+++ src/infra/adapters/jwt-adapter.ts
@@ -3,8 +3,8 @@ import jsonwebtoken from 'jsonwebtoken'
 import { JSONType } from "@/data/model";
 
 export class JwtAdapter {
-  encrypt (payload: JSONType): void {
+  encrypt (payload: JSONType): string {
     const secretKey = process.env.SECRET_KEY!
-    jsonwebtoken.sign(Buffer.from(JSON.stringify(payload)), secretKey)
+    return jsonwebtoken.sign(Buffer.from(JSON.stringify(payload)), secretKey)
   }
 }
