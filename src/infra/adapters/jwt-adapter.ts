1034fadd8b796813c9e8d63163b8b37310bce03b
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-02 16:30:04 +0100

    refactor: add Encrypt contract on JwtAdapter

--- src/infra/adapters/jwt-adapter.ts
+++ src/infra/adapters/jwt-adapter.ts
@@ -2,8 +2,8 @@ import jsonwebtoken from 'jsonwebtoken'
 
 import { JSONType } from "@/data/model";
 import { JwtAdapterError, NoSecretFoundError } from '@/application/errors/errors';
-
-export class JwtAdapter {
+import { Encrypt } from "@/data/domain"
+export class JwtAdapter implements Encrypt<JSONType> {
   encrypt (payload: JSONType): string {
     const secretKey = process.env.SECRET_KEY!
     if (secretKey === undefined) throw new NoSecretFoundError()
