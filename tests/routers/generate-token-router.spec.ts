8d628ab5bdf8d4dba3c1984c296a4e2d4ad01949
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-02 19:38:07 +0100

    test: ensure generateTokenRouters returns 400 if missing param surname

--- tests/routers/generate-token-router.spec.ts
+++ tests/routers/generate-token-router.spec.ts
@@ -11,6 +11,20 @@ describe('Generate Token Router', () => {
       expect(httpResponse.body.errors[0].path).toContain('name')
   })
 
+  it('should returns 400 if missing param surname', async () => {
+    const httpResponse = await request(createApp())
+      .post('/api/token')
+      .send({})
+      .expect(400)
+      expect(httpResponse.body.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            path: expect.arrayContaining(['surname'])
+          })
+        ])
+      )
+  })
+
   it('should returns 400 if missing param email', async () => {
     const httpResponse = await request(createApp())
       .post('/api/token')
