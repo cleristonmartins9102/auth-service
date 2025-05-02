f40d0614fe04b835551f1c4ff50a3dfa3e01d33f
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-02 17:05:45 +0100

    test: ensure GenerateTokenController returns correct token

--- tests/controllers/generate-token-controller.spec.ts
+++ tests/controllers/generate-token-controller.spec.ts
@@ -7,18 +7,30 @@ import { faker } from "@faker-js/faker/."
 
 describe('GenerateTokenController', () => {
   const jtwAdapter = mock<Encrypt<JSONType>>()
-  
+  const httpRequest = { 
+    body: { name: faker.person.firstName() },
+    params: {},
+    query: {}
+  }
+
+  beforeAll(() => {
+    jtwAdapter.encrypt.mockReturnValue('generatedToken')
+  })
+
   it('should call JwtAdapter.encrypt with correct value', async () => {
-    const httpRequest = { 
-      body: { name: faker.person.firstName() },
-      params: {},
-      query: {}
-    }
     const sut = new GenerateTokenController(jtwAdapter)
 
     await sut.perform(httpRequest)
 
     expect(jtwAdapter.encrypt).toHaveBeenCalled()
     expect(jtwAdapter.encrypt).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  it('should returns the same value received from JWTAdapter.encrypt', async () => {
+    const sut = new GenerateTokenController(jtwAdapter)
+
+    const response = await sut.perform(httpRequest)
+
+    expect(response).toEqual({ statusCode: 200, body: 'generatedToken'})
   })
 })
