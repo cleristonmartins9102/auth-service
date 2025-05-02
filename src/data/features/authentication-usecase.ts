f09c4b70130729d89bf05dd7d40af52672ddb212
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-02 21:38:12 +0100

    feat: ensure AuthenticationUseCase returns the same value received from UserService

--- src/data/features/authentication-usecase.ts
+++ src/data/features/authentication-usecase.ts
@@ -1,10 +1,12 @@
 import { UserNotFoundError } from "@/application/errors/errors";
 import { Auth, GetUserByEmail } from "@/data/domain";
+import { UserModel } from "../model";
 
-export class AuthenticationUseCase {
+export class AuthenticationUseCase implements Auth {
   constructor (private readonly userService: GetUserByEmail) {}
-  async auth(params: Auth.Params): Promise<void> {
+  async auth(params: Auth.Params): Promise<UserModel> {
     const user = await this.userService.getByEmail(params.email)
     if (null === user) throw new UserNotFoundError('email', params.email)
+    return user
   }
 }
