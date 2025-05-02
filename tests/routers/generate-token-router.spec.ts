02f2a96cf005ef7cdc5e7bae3dbdf62ad3bff9f3
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-02 17:57:11 +0100

    test: ensure generateTokenRouter returns 400 if missing param name

---
+++ tests/routers/generate-token-router.spec.ts
@@ -0,0 +1,13 @@
+import { createApp } from '@/main/config/app'
+import request from 'supertest'
+
+describe('Generate Token Router', () => {
+  process.env.SECRET_KEY = '123456'
+  it('should returns 401 if missing param name', async () => {
+    const httpResponse = await request(createApp())
+      .post('/api/token')
+      .send({})
+      .expect(400)
+      expect(httpResponse.body.errors[0].path).toContain('name')
+  })
+})
