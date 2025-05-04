1f0ca3c9cc7c9d963e2e99eb9746377dcad9990b
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-04 21:41:24 +0100

    feat: ensure AuthenticationUseCase returns correct value

--- src/data/domain/auth.ts
+++ src/data/domain/auth.ts
@@ -1,3 +1,5 @@
+import { UserModel } from "../model"
+
 export interface Auth {
   auth (params: Auth.Params): Promise<Auth.Result>
 }
@@ -6,6 +8,7 @@ export namespace Auth {
   export type Result = {
     token: string
     refreshToken: string
+    payload: UserModel & { iat: number }
   }
   export type Params = {
     email: string
--- src/data/features/authentication-usecase.ts
+++ src/data/features/authentication-usecase.ts
@@ -7,14 +7,14 @@ export class AuthenticationUseCase implements Auth {
     private readonly fsCredentialsRepository: GetCredentialsByEmail,
     private readonly userService: GetUserByEmail,
     private readonly bcryptAdapter: Compare,
-    private readonly jwtAdapter: Decrypt<CredentialsModel>,
+    private readonly jwtAdapter: Decrypt<UserModel & { iat: number }>,
   ) {}
-  async auth(params: Auth.Params): Promise<UserModel> {
+  async auth(params: Auth.Params): Promise<Auth.Result> {
     const credentials = await this.fsCredentialsRepository.getByEmail(params.email)
     if (null === credentials) throw new CredentialsNotFoundError('email', params.email)
     const response = await this.bcryptAdapter.compare(params.password, credentials.password)
     if (false === response) throw new WrongPasswordError()
     const tokenPayload = this.jwtAdapter.decrypt(credentials.token)
-    return '' as any
+    return { token: credentials.token, refreshToken: credentials.refreshToken, payload: tokenPayload }
   }
 }
