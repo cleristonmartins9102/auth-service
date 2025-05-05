b9eabe33a7bd4fe075511f95fcf7cfe506f5600f
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-05 13:10:59 +0100

    feat: ensure calls sign with correct expire value

--- src/infra/adapters/jwt-adapter.ts
+++ src/infra/adapters/jwt-adapter.ts
@@ -1,16 +1,26 @@
 import jsonwebtoken from 'jsonwebtoken'
 
 import { UserModel } from "@/data/model";
-import { JwtAdapterError, NoSecretFoundError } from '@/application/errors/errors';
-import { Encrypt } from "@/data/domain"
-export class JwtAdapter implements Encrypt<UserModel> {
-  encrypt (payload: UserModel): string {
+import { ExpiredTokenError, JwtAdapterError, NoSecretFoundError } from '@/application/errors/errors';
+import { Decrypt, Encrypt } from "@/data/domain"
+export class JwtAdapter implements Encrypt<UserModel>, Decrypt<UserModel> {
+  encrypt (payload: UserModel, expireAt: Encrypt.ExpireAt): string {
     const secretKey = process.env.SECRET_KEY!
     if (undefined === secretKey) throw new NoSecretFoundError()
       try {
-        return jsonwebtoken.sign(payload as object, secretKey, { expiresIn: '1h' }) 
+        return jsonwebtoken.sign(payload as object, secretKey, { expiresIn: expireAt }) 
       } catch (error) {
         throw new JwtAdapterError(error as Error)
       }
+  }
+
+  decrypt(value: string): UserModel & { iat: number } {
+    const secretKey = process.env.SECRET_KEY!
+    if (undefined === secretKey) throw new NoSecretFoundError()
+    try {
+      return jsonwebtoken.verify(value, secretKey) as any
+    } catch (error) {
+      throw new ExpiredTokenError(error as Error)
+    }
   }
 }
