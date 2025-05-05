92acd4b5360b0e84591a9ddd3b26ecbc2aa6122e
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-05 12:35:47 +0100

    test: ensure RefreshTokenUsecase calls JwtAdapter.decrypt with correct token

--- tests/feature/refresh-token-usecase.spec.ts
+++ tests/feature/refresh-token-usecase.spec.ts
@@ -1,11 +1,20 @@
-import { GetCredentialsByRefreshToken, RefreshToken } from "@/data/domain"
+import { Decrypt, Encrypt, GetCredentialsByRefreshToken, RefreshToken, UpdateCredentialRepository } from "@/data/domain"
 import { RefreshTokenUsecase } from "@/data/features"
+import { UserModel } from "@/data/model"
 import mock from "jest-mock-extended/lib/Mock"
+import { makeCredentialsStub } from "../../tests/stubs"
 
 describe('RefreshTokenUsecase', () => {
-  const fsCredentialRepository = mock<GetCredentialsByRefreshToken>()
+  const fsCredentialRepository = mock<GetCredentialsByRefreshToken & UpdateCredentialRepository>()
+  const jwtAdapter = mock<Encrypt<UserModel> & Decrypt<UserModel>>()
+  const credential = makeCredentialsStub()
+
+  beforeAll(() => {
+    fsCredentialRepository.getByRefreshToken.mockResolvedValue(credential)
+  })
+
   it('should call fsCredentialRepository.getByRefreshToken', async () => {
-    const sut = new RefreshTokenUsecase(fsCredentialRepository)
+    const sut = new RefreshTokenUsecase(fsCredentialRepository, jwtAdapter)
 
     await sut.refresh('refreshTokenValue')
 
@@ -15,10 +24,19 @@ describe('RefreshTokenUsecase', () => {
 
   it('should throw CredentialNotFoundError if fsCredentialRepository returns null', async () => {
     fsCredentialRepository.getByRefreshToken.mockResolvedValueOnce(null)
-    const sut = new RefreshTokenUsecase(fsCredentialRepository)
+    const sut = new RefreshTokenUsecase(fsCredentialRepository, jwtAdapter)
 
     const response = sut.refresh('refreshTokenValue')
 
     await expect(response).rejects.toThrow()
+  })
+
+  it('should call JwtAdapter.decrypt with correct token', async () => {
+    const sut = new RefreshTokenUsecase(fsCredentialRepository, jwtAdapter)
+
+    await sut.refresh('refreshTokenValue')
+
+    expect(jwtAdapter.decrypt).toHaveBeenCalled()
+    expect(jwtAdapter.decrypt).toHaveBeenCalledWith(credential.token)
   })
 })
