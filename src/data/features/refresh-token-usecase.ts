ac8a0229af54568e034960ad10f9718ca2df01a9
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-05 12:49:14 +0100

    feat: ensure RefreshTokenUsecase returns correct value

--- src/data/features/refresh-token-usecase.ts
+++ src/data/features/refresh-token-usecase.ts
@@ -2,17 +2,21 @@ import { CredentialsNotFoundError } from "@/application/errors/errors";
 import { Decrypt, Encrypt, GetCredentialsByRefreshToken, RefreshToken, UpdateCredentialRepository } from "../domain";
 import { UserModel } from "../model";
 
-export class RefreshTokenUsecase {
+export class RefreshTokenUsecase implements RefreshToken {
   constructor (
     private readonly fsCredentialRepository: GetCredentialsByRefreshToken & UpdateCredentialRepository,
     private readonly jwtAdapter: Decrypt<UserModel> & Encrypt<UserModel>
   ) {}
-  async refresh (refreshToken: string): Promise<void> {
+  async refresh (refreshToken: string): Promise<RefreshToken.Return> {
     const credential = await this.fsCredentialRepository.getByRefreshToken(refreshToken)
     if (credential === null) throw new CredentialsNotFoundError('refreshToken', refreshToken)
     const tokenPayload = this.jwtAdapter.decrypt(credential.token)
     const updatedToken = this.jwtAdapter.encrypt(tokenPayload)
     const updatedRefreshToken = this.jwtAdapter.encrypt(tokenPayload)
     await this.fsCredentialRepository.update(credential.id, { token: updatedToken, refreshToken: updatedRefreshToken })
+    return {
+      token: updatedToken,
+      refreshToken: updatedRefreshToken
+    }
   }
 }
