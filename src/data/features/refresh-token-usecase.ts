701a012dcfb5adadaafc91ba3542fd52afe80113
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-05 12:38:59 +0100

    feat: ensure RefreshTokenUsecase calls JwtAdapter.encrypt with correct token payload

--- src/data/features/refresh-token-usecase.ts
+++ src/data/features/refresh-token-usecase.ts
@@ -5,11 +5,12 @@ import { UserModel } from "../model";
 export class RefreshTokenUsecase {
   constructor (
     private readonly fsCredentialRepository: GetCredentialsByRefreshToken,
-    private readonly jwtAdapter: Decrypt<UserModel>
+    private readonly jwtAdapter: Decrypt<UserModel> & Encrypt<UserModel>
   ) {}
   async refresh (refreshToken: string): Promise<void> {
     const credential = await this.fsCredentialRepository.getByRefreshToken(refreshToken)
     if (credential === null) throw new CredentialsNotFoundError('refreshToken', refreshToken)
-    this.jwtAdapter.decrypt(credential.token)
+    const tokenPayload = this.jwtAdapter.decrypt(credential.token)
+    this.jwtAdapter.encrypt(tokenPayload)
   }
 }
