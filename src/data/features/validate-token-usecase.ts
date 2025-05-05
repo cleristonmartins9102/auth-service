213900afe308b559a9d953e0d48c9fbb715b92f1
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-05 14:31:13 +0100

    feat: ensure ValidateTokenUsecase calls RefreshTokenUsecase if token is expired

--- src/data/features/validate-token-usecase.ts
+++ src/data/features/validate-token-usecase.ts
@@ -1,10 +1,20 @@
-import { Decrypt } from "../domain";
+import { ExpiredTokenError } from "@/application/errors/errors";
+import { Decrypt, RefreshToken } from "../domain";
 
 export class ValidateTokenUsecase {
   constructor (
-    private readonly jwtAdapter: Decrypt
+    private readonly jwtAdapter: Decrypt,
+    private readonly refreshTokenUsecase: RefreshToken
   ) {}
-  async validate (token: string): Promise<void> {
-    this.jwtAdapter.decrypt(token)
+  async validate (token: string, refreshToken: string): Promise<void> {
+    try {
+      this.jwtAdapter.decrypt(token)
+    } catch (error) {
+      if (error instanceof ExpiredTokenError) {
+        await this.refreshTokenUsecase.refresh(refreshToken)
+      } else {
+        throw error
+      }
+    }
   }
 }
