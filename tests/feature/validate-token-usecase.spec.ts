2f9c32264af4aff7a08a390ce0dc014b854f8803
Cleriston Martins Cardoso <46527150+cleristonmartins9102@users.noreply.github.com>
2025-05-05 14:31:16 +0100

    test: ensure ValidateTokenUsecase calls RefreshTokenUsecase if token is expired

--- tests/feature/validate-token-usecase.spec.ts
+++ tests/feature/validate-token-usecase.spec.ts
@@ -1,15 +1,18 @@
-import { Decrypt } from "@/data/domain"
+import { ExpiredTokenError } from "@/application/errors/errors"
+import { Decrypt, RefreshToken } from "@/data/domain"
 import { ValidateTokenUsecase } from "@/data/features/validate-token-usecase"
 import mock from "jest-mock-extended/lib/Mock"
 
 describe('ValidateTokenUsecase', () => {
   const token = 'userToken'
+  const refreshToken = 'refreshToken'
   const jwtAdapter = mock<Decrypt>()
+  const refreshTokenUsecase = mock<RefreshToken>()
 
   it('should call JwtAdapter.decrypt with correct token', async () => {
-    const sut = new ValidateTokenUsecase(jwtAdapter)
+    const sut = new ValidateTokenUsecase(jwtAdapter, refreshTokenUsecase)
 
-    await sut.validate(token)
+    await sut.validate(token, refreshToken)
 
     expect(jwtAdapter.decrypt).toHaveBeenCalled()
     expect(jwtAdapter.decrypt).toHaveBeenCalledWith(token)
@@ -21,12 +24,27 @@ describe('ValidateTokenUsecase', () => {
     })
     let error
     try {
-      const sut = new ValidateTokenUsecase(jwtAdapter)
-      await sut.validate(token)
+      const sut = new ValidateTokenUsecase(jwtAdapter, refreshTokenUsecase)
+      await sut.validate(token, refreshToken)
     } catch (err) {
       error = err
     }
 
     expect(error).toBeInstanceOf(Error)
+  })
+
+  it('should call refreshTokenUsecase if token has expired', async () => {
+    jwtAdapter.decrypt.mockImplementationOnce(() => {
+      throw new ExpiredTokenError(new Error())
+    })
+    let error
+    try {
+      const sut = new ValidateTokenUsecase(jwtAdapter, refreshTokenUsecase)
+      await sut.validate(token, refreshToken)
+    } catch (err) {
+      error = err
+    }
+
+    expect(refreshTokenUsecase.refresh).toHaveBeenCalled()
   })
 })
